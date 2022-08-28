d67d041c7b2c0bbf95e45d3a9fec7ca833257398
sougata <bagchi.sougata@gmail.com>
2022-08-28 11:43:24 +0530

    First commit

---
+++ eclipse-workspace/spring-ocp-1/src/main/java/com/example/openshift/api/demo/SpringOcp1Application.java
@@ -0,0 +1,28 @@
+package com.example.openshift.api.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringOcp1Application {
+
+	@GetMapping("/")
+	public String welcome() {
+		return "Welcome to Java Springboot";
+	}
+
+	@GetMapping("/{input}")
+	public String congrats(@PathVariable String input) {
+		return "Welcome" + input;
+
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOcp1Application.class, args);
+	}
+
+}
---
+++ eclipse-workspace/spring-ocp-1/src/test/java/com/example/openshift/api/demo/SpringOcp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openshift.api.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOcp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
